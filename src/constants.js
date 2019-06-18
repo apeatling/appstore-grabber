70fd5c8fe55961030aceaa7ee4d6c46d16b650a3
apeatling <apeatling@gmail.com>
2019-06-18 09:58:29 -0700

    Update constants to support dev and prod settings together.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,12 @@
-export const API_URL = "https://itunes.apple.com/search"
-export const RENDER_URL = "http://localhost:3000"
 export const UP_ARROW_KEY = 38;
 export const DOWN_ARROW_KEY = 40;
 export const ESC_KEY = 27;
 export const RETURN_KEY = 13;
+
+// Prod
+export const API_URL = "https://getappstore.page/api"
+export const RENDER_URL = "https://getappstore.page/render"
+
+// Dev
+// export const API_URL = "https://itunes.apple.com/search"
+// export const RENDER_URL = "http://localhost:8888/render"
